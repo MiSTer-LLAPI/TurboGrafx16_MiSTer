10ba7f2d47c9cd33c59d7474a0390dc476bd1296
onthewaveagain <62063840+onthewaveagain@users.noreply.github.com>
2025-02-05 22:20:43 +0000

    New LLAPI release

--- TurboGrafx16.sv
+++ TurboGrafx16.sv
@@ -233,6 +233,12 @@ video_freak video_freak
 `include "build_id.v"
 parameter CONF_STR = {
 	"TGFX16;;",
+	//LLAPI: OSD menu item
+	//LLAPI Always ON
+	"-,>> LLAPI enabled core    <<;",
+	"-,>> Connect USER I/O port <<;",
+	"-;",
+	//END LLAPI	
 	"FS0,PCEBIN,Load TurboGrafx;",
 `ifndef DEBUG_BUILD
 	"FS1,SGX,Load SuperGrafx;",
@@ -244,15 +250,11 @@ parameter CONF_STR = {
 `endif
 	"S0,CUECHD,Insert CD;",
 	"-;",
-	//LLAPI: OSD menu item
-	//LLAPI Always ON
-	"-,<< LLAPI enabled >>;",
-	"-,<< Use USER I/O port >>;",
-	"oUV,LLAPI Remap (III) btn ,OFF,START,SELECT;",
-	"-;",
-	//END LLAPI	
+
 	"C,Cheats;",
 	"H1O5,Cheats enabled,ON,OFF;",
+	"-;",
+	"oUV,LLAPI (III) mapping,III,START,SELECT;",
 	"-;",
 	"D0RG,Load Backup RAM;",
 	"D0R7,Save Backup RAM;",
@@ -972,41 +974,51 @@ always @(posedge clk_sys) begin
 	end
 end
 
-//////////////////   LLAPI   ///////////////////
+///////////////////   LLAPI   ///////////////////
 
 wire [31:0] llapi_buttons, llapi_buttons2;
 wire [71:0] llapi_analog, llapi_analog2;
 wire [7:0]  llapi_type, llapi_type2;
 wire llapi_en, llapi_en2;
-
-wire llapi_select = 1'b1;
-
 wire llapi_latch_o, llapi_latch_o2, llapi_data_o, llapi_data_o2;
+wire [11:0] joy_ll_a;
+wire [11:0] joy_ll_b;
+wire [15:0] joy_0;
+wire [15:0] joy_1;
+wire [15:0] joy_2;
+wire [15:0] joy_3;
+wire [15:0] joy_4;
 
-//Connection to USER_OUT port
+
+//Assign (DOWN + START + FIRST BUTTON) Combinaison to bring the OSD up - P1 and P2 ports.
+wire llapi_osd = (llapi_buttons[26] & llapi_buttons[5] & llapi_buttons[0]) || (llapi_buttons2[26] & llapi_buttons2[5] & llapi_buttons2[0]);
+
+// LLAPI Indexes:
+// 0 = D+    = P1 Latch
+// 1 = D-    = P1 Data
+// 2 = TX-   = LLAPI Enable
+// 3 = GND_d = N/C
+// 4 = RX+   = P2 Latch
+// 5 = RX-   = P2 Data
+
 always_comb begin
-	USER_OUT = 6'b111111;
-	if (llapi_select) begin
 		USER_OUT[0] = llapi_latch_o;
 		USER_OUT[1] = llapi_data_o;
-		USER_OUT[2] = ~(llapi_select & ~OSD_STATUS); // Blister LED
+		USER_OUT[2] = OSD_STATUS; // Blister LED
 		USER_OUT[4] = llapi_latch_o2;
 		USER_OUT[5] = llapi_data_o2;
-	end else if (snac & ~OSD_STATUS) begin
-		USER_OUT = {2'b11, snac_clr, 1'b1, snac_sel, 2'b11}; //conf for SNAC support
-	end
 end
 
 //Port 1 conf
 LLAPI llapi
 (
 	.CLK_50M(CLK_50M),
-	.LLAPI_SYNC(VSync),
+	.LLAPI_SYNC(vblank),
 	.IO_LATCH_IN(USER_IN[0]),
 	.IO_LATCH_OUT(llapi_latch_o),
 	.IO_DATA_IN(USER_IN[1]),
 	.IO_DATA_OUT(llapi_data_o),
-	.ENABLE(llapi_select & ~OSD_STATUS),
+	.ENABLE(~OSD_STATUS), // Disable LLAPI mode when Core OSD is open
 	.LLAPI_BUTTONS(llapi_buttons),
 	.LLAPI_ANALOG(llapi_analog),
 	.LLAPI_TYPE(llapi_type),
@@ -1017,46 +1029,23 @@ LLAPI llapi
 LLAPI llapi2
 (
 	.CLK_50M(CLK_50M),
-	.LLAPI_SYNC(VSync),
+	.LLAPI_SYNC(vblank),
 	.IO_LATCH_IN(USER_IN[4]),
 	.IO_LATCH_OUT(llapi_latch_o2),
 	.IO_DATA_IN(USER_IN[5]),
 	.IO_DATA_OUT(llapi_data_o2),
-	.ENABLE(llapi_select & ~OSD_STATUS),
+	.ENABLE(~OSD_STATUS), // Disable LLAPI mode when Core OSD is open
 	.LLAPI_BUTTONS(llapi_buttons2),
 	.LLAPI_ANALOG(llapi_analog2),
 	.LLAPI_TYPE(llapi_type2),
 	.LLAPI_EN(llapi_en2)
 );
 
-reg llapi_button_pressed, llapi_button_pressed2;
-
-always @(posedge CLK_50M) begin
-        if (reset) begin
-                llapi_button_pressed  <= 0;
-                llapi_button_pressed2 <= 0;
-	end else begin
-		if (|llapi_buttons)
-                	llapi_button_pressed  <= 1;
-        	if (|llapi_buttons2)
-                	llapi_button_pressed2 <= 1;
-	end
-end
-
 // controller id is 0 if there is either an Atari controller or no controller
-// if id is 0, assume there is no controller until a button is pressed
-// also check for 255 and treat that as 'no controller' as well
-wire use_llapi  = llapi_en  && llapi_select && ((|llapi_type  && ~(&llapi_type))  || llapi_button_pressed);
-wire use_llapi2 = llapi_en2 && llapi_select && ((|llapi_type2 && ~(&llapi_type2)) || llapi_button_pressed2);
-
-// Indexes:
-// 0 = D+    = P1 Latch
-// 1 = D-    = P1 Data
-// 2 = TX-   = LLAPI Enable
-// 3 = GND_d = N/C
-// 4 = RX+   = P2 Latch
-// 5 = RX-   = P2 Data
-
+// if id is 0, assume there is no controller
+// also check for 255 ('Searching mode') and treat that as 'no controller' as well
+wire use_llapi  = llapi_en && ((|llapi_type  && ~(&llapi_type))); //  || llapi_button_pressed);
+wire use_llapi2 = llapi_en2 && ((|llapi_type2 && ~(&llapi_type2))); // || llapi_button_pressed2);
 
 //Controller string provided by core for reference (order is important)
 //Controller specific mapping based on type. More info here : https://docs.google.com/document/d/12XpxrmKYx_jgfEPyw-O2zex1kTQZZ-NSBdLO2RQPRzM/edit
@@ -1064,7 +1053,6 @@ wire use_llapi2 = llapi_en2 && llapi_select && ((|llapi_type2 && ~(&llapi_type2)
 
 //Port 1 mapping
 
-wire [11:0] joy_ll_a;
 always_comb begin
         // button layout for genesis controllers
         if (llapi_type == 20 || llapi_type == 21) begin
@@ -1118,7 +1106,6 @@ end
 
 //Port 2 mapping
 
-wire [11:0] joy_ll_b;
 always_comb begin
         // button layout for genesis controllers
         if (llapi_type2 == 20 || llapi_type2 == 21) begin
@@ -1154,32 +1141,22 @@ always_comb begin
         end
 end
 
-//Assign (DOWN + START + FIRST BUTTON) Combinaison to bring the OSD up - P1 and P2 ports.
-//TODO : Support long press detection
-wire llapi_osd = (llapi_buttons[26] & llapi_buttons[5] & llapi_buttons[0]) || (llapi_buttons2[26] & llapi_buttons2[5] & llapi_buttons2[0]);
-
-// if LLAPI is enabled, shift USB controllers over to the next available player slot
-wire [11:0] joy_0, joy_1, joy_2, joy_3, joy_4;
+// Player / LLAPI port allocation
 always_comb begin
-        if (use_llapi & use_llapi2) begin
+        if (~use_llapi & use_llapi2) begin
+               	joy_0 = joy_ll_b;
+                joy_1 = joy_usb_0;
+				joy_2 = joy_usb_1;
+				joy_3 = joy_usb_2;
+				joy_4 = joy_usb_3;
+            
+        end else begin
                 joy_0 = joy_ll_a;
                 joy_1 = joy_ll_b;
-                joy_2 = joy_usb_0;
-                joy_3 = joy_usb_1;
-                joy_4 = joy_usb_2;
-        end else if (use_llapi ^ use_llapi2) begin
-                joy_0 = use_llapi  ? joy_ll_a : joy_usb_0;
-                joy_1 = use_llapi2 ? joy_ll_b : joy_usb_0;
-                joy_2 = joy_usb_1;
-                joy_3 = joy_usb_2;
-                joy_4 = joy_usb_3;
-        end else begin
-                joy_0 = joy_usb_0;
-                joy_1 = joy_usb_1;
-                joy_2 = joy_usb_2;
-                joy_3 = joy_usb_3;
-                joy_4 = joy_usb_4;
-        end
+				joy_2 = joy_usb_0;
+				joy_3 = joy_usb_1;
+				joy_4 = joy_usb_2;
+		end
 end
 
 ////////////////////////////  CODES  ///////////////////////////////////
